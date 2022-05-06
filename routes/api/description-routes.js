b43c88f4d4c38230fc37bd69b845474e91da7df7
Your Name <felix.acevedo.tech@gmail.com>
2022-05-06 09:41:01 -0400

    working on controller/api and routes/api

---
+++ routes/api/description-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Description } = require('../../models');
+
+// get all descriptions
+
+// post/ create new description
+
+// delete description by id
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { User, Post, Description, Search } = require('../../models');
+
+// get all users
+
+// get user by id
+
+// post/create
+
+// post /login findOne with password check
+
+// put/update 
+
+// delete user by id
+
+module.exports = router;
