f840ded26608d6b47c2d81b335397b0a19025261
Chlovanna <ashdmt39@gmail.com>
2022-05-07 17:56:51 -0400

    added consultant and employer-route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./consultant-routes.js');
 const postRoutes = require('./post-routes.js');
 const descriptionRoutes = require('./description-routes');
 
