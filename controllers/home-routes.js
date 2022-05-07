d43a288856c83de3583435b78be1df60e7d4de74
Chlovanna <ashdmt39@gmail.com>
2022-05-07 10:50:06 -0400

    add employer and consultant nav link to main handlebar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,12 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+router.get("/employer", (req, res) => {
+  req.session.employerLoggedIn
+      ?  res.render("employer")
+      : res.redirect('/login')
+});
+
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
