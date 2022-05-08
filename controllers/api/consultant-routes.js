983cd14513b47d37ee8fce29f43b17ab44f53abf
Chlovanna <ashdmt39@gmail.com>
2022-05-08 16:02:30 -0400

    setup consultant-routes

--- controllers/api/consultant-routes.js
+++ controllers/api/consultant-routes.js
@@ -1,85 +1,85 @@
-// const router = require('express').Router();
-// const { User, Post, Description, Search } = require('../../models');
+const router = require('express').Router();
+const { Consultant } = require('../../models');
 
-// //GET all /api/user/
-// router.get('/', (req, res) => {
-//   User.findAll({
-//     attributes: { exclude: ['password'] }
-//   })
-//     .then(dbUserData => res.json(dbUserData))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+//GET all /api/consultant/
+router.get('/', (req, res) => {
+  Consultant.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// //GET single user /api/user/:id
+//GET single consultant /api/consultant/:id
 
-// //POST /api/user/
-// router.post('/', (req, res) => {
-//   User.create({
-//     username: req.body.username,
-//     email: req.body.email,
-//     password: req.body.password
-//   })
-//     .then(dbUserData => {
-//       req.session.save(() => {
-//         req.session.userId = dbUserData.id;
-//         req.session.username = dbUserData.username;
-//         req.session.employerloggedIn = true;
+//POST /api/consultant/
+router.post('/', (req, res) => {
+  Consultant.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.userId = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.consultantloggedIn = true;
 
-//         res.json(dbUserData);
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// // POST /api/user/login
-// router.post('/login', (req, res) => {
-//   User.findOne({
-//     where: {
-//       username: req.body.username,
-//       password: req.body.password
-//     }
-//   }).then(dbUserData => {
-//     if (!dbUserData) {
-//       res.status(400).json({ message: 'No user account found!' });
-//       return;
-//     }
+// POST /api/consultant/login
+router.post('/login', (req, res) => {
+  Consultant.findOne({
+    where: {
+      username: req.body.username,
+      password: req.body.password
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user account found!' });
+      return;
+    }
 
-//     const validPassword = dbUserData.checkPassword(req.body.password);
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
-//     if (!validPassword) {
-//       res.status(400).json({ message: 'Incorrect password!' });
-//       return;
-//     }
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
 
-//     req.session.save(() => {
-//       req.session.userId = dbUserData.id;
-//       req.session.username = dbUserData.username;
-//       req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.userId = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.consultantloggedIn = true;
 
-//       res.json({ user: dbUserData, message: 'You are now logged in!' });
-//     });
-//   });
-// });
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+});
 
-// // log out route
-// // POST /api/user/logout
-// router.post('/logout', (req, res) => {
-//   if (req.session.loggedIn) {
-//     req.session.destroy(() => {
-//       res
-//         .status(204)
-//         .json({ message: 'You are now logged out!' })
-//         .end();
-//     });
-//   } else {
-//     res.status(400).end();
-//   }
-// });
+// log out route
+// POST /api/consultant/logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res
+        .status(204)
+        .json({ message: 'You are now logged out!' })
+        .end();
+    });
+  } else {
+    res.status(400).end();
+  }
+});
 
-// module.exports = router;
+module.exports = router;
