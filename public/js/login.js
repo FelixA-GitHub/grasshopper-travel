1a77cb3204368118e5d4246d6a0cc046fabcb40f
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-05-09 23:57:10 -0400

    adjusted login post request

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event) {
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('/api/consultant/login', {
       method: 'post',
       body: JSON.stringify({
         username,
@@ -15,7 +15,8 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/');
+      console.log("Logged in!");
     } else {
       alert(response.statusText);
     }
