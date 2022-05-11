624f60598bdfca706706028629689f2c15ff6a30
Your Name <felix.acevedo.tech@gmail.com>
2022-05-11 15:27:31 -0400

    updated application route name

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,4 +30,15 @@ router.get("/application", (req, res) => {
   res.render("application", { session : req.session });
 });
 
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
--- public/js/jobHandler.js
+++ public/js/jobHandler.js
@@ -11,7 +11,7 @@ fetch("/api/jobs")
   </form>
   
             <div class="jobCard">
-             <a href="/applicaiton">   <h2>${job.title}</h2> </a>
+             <a href="/application">   <h2>${job.title}</h2> </a>
                 <p> ${job.location} | ${job.duration} | ${job.salary}</p>
 
                 <p> ${job.job_description}</p>
