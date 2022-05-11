0059231580b5bdf31874254ab478c002fcbd66b0
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-05-11 19:51:03 -0400

    added job view code to generate HTML

--- public/js/viewjob.js
+++ public/js/viewjob.js
@@ -1,5 +1,8 @@
 import { getJobById } from '../utils/job-api.js';
 
+let applyButton;
+let backButton;
+
 document.addEventListener('DOMContentLoaded', async () => {
     const location = window.location.pathname;
     const split = location.split("/");
@@ -7,33 +10,34 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log(currentJob);
 
     // write code to generate html based on currentJob
-    let cont = document.getElementById("container");
-    let title = document.createElement("p");
-    let desc = document.createElement("p");
-    title.textContent = currentJob.title;
-    desc.textContent = currentJob.job_description;
-    cont.appendChild(title);
-    cont.appendChild(desc);
+    var content = document.getElementById("job-posting");
+    content.innerHTML = 
+    `<h3 class="title">${currentJob.title}</h3>
+    <hr></hr>
+    <p class="m-5 is-size-5">${currentJob.job_description}</p>
+    <p class="is-size-4">Salary: ${currentJob.salary}</p>
+    <p class="is-size-5 pt-2">Location: ${currentJob.location}</p>
+    <p class="is-size-5 pt-4">${currentJob.job_type}</p>
+    <p class="is-size-6 pb-6">Duration: ${currentJob.duration}</p>
+
+    <button class="button is-normal is-success" id="apply-button">Apply Now</button>
+    <button class="button is-normal" id="back-button">Back to Dashboard</button>
+
+    <p class="is-size-7 pt-6">Job posted: 05/11/22</p>`
+    ;
+    applyButton = document.getElementById("apply-button");
+    backButton = document.getElementById("back-button");
+
+    backButton.addEventListener("click", function () {
+        document.location.replace(`/dashboard`);
+    });
+
+    applyButton.addEventListener("click", function () {
+        document.location.replace(`/application`);
+    });
 })
-// this needs to show job by id when title is clicked
-// fetch("/api/jobs")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     dataArr = data;
 
-//     data.forEach((job) => {
-//       document.querySelector(".JobContainer").innerHTML += `
-      
-//       <form>
-//       <input type="hidden" name="avatar_url" id="${job.title}" class="simple-file-upload"> 
-//   </form>
-  
-//             <div class="jobCard">
-//              <a href="/application">   <h2>${job.title}</h2> </a>
-//                 <p> ${job.location} | ${job.duration} | ${job.salary}</p>
 
-//                 <p> ${job.job_description}</p>
-//             </div>
-//         `;
-//     });
-//   });
+
+
+
