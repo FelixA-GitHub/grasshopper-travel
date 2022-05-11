9db4c2728a84de80f4e680620d89aab0a17eb46c
Your Name <felix.acevedo.tech@gmail.com>
2022-05-11 16:21:45 -0400

    working on improving application and viewjob

--- public/js/viewjob.js
+++ public/js/viewjob.js
@@ -8,3 +8,25 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // write code to generate html based on currentJob
 })
+// this needs to show full job by id when title is clicked
+// fetch("/api/jobs")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     dataArr = data;
+
+//     data.forEach((job) => {
+//       document.querySelector(".jobContainer").innerHTML += `
+      
+//       <form>
+//       <input type="hidden" name="avatar_url" id="${job.title}" class="simple-file-upload"> 
+//   </form>
+  
+//             <div class="jobCard">
+//              <a href="/application">   <h2>${job.title}</h2> </a>
+//                 <p> ${job.location} | ${job.duration} | ${job.salary}</p>
+
+//                 <p> ${job.job_description}</p>
+//             </div>
+//         `;
+//     });
+//   });
