7ace12b322ff0eb7d271710c85c7ae983dd96a69
Chlovanna <ashdmt39@gmail.com>
2022-05-11 13:34:32 -0400

    create jobhandler

--- public/js/jobHandler.js
+++ public/js/jobHandler.js
@@ -3,14 +3,13 @@ fetch("/api/jobs")
   .then((data) => {
     dataArr = data;
 
-    
-
     data.forEach((job) => {
       document.querySelector(".jobContainer").innerHTML += `
       
       <form>
       <input type="hidden" name="avatar_url" id="${job.title}" class="simple-file-upload"> 
   </form>
+  
             <div class="jobCard">
              <a href="/applicaiton">   <h2>${job.title}</h2> </a>
                 <p> ${job.location} | ${job.duration} | ${job.salary}</p>
