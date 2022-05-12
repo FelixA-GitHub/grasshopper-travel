2bf0731dc06464530e52ad35a8899f7022cbe92c
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-05-11 21:12:09 -0400

    started created job page

--- public/js/viewjob.js
+++ public/js/viewjob.js
@@ -18,12 +18,10 @@ document.addEventListener('DOMContentLoaded', async () => {
     <p class="is-size-4">Salary: ${currentJob.salary}</p>
     <p class="is-size-5 pt-2">Location: ${currentJob.location}</p>
     <p class="is-size-5 pt-4">${currentJob.job_type}</p>
-    <p class="is-size-6 pb-6">Duration: ${currentJob.duration}</p>
+    <p class="is-size-6 pb-3">Duration: ${currentJob.duration}</p>
 
-    <button class="button is-normal is-success" id="apply-button">Apply Now</button>
-    <button class="button is-normal" id="back-button">Back to Dashboard</button>
 
-    <p class="is-size-7 pt-6">Job posted: 05/11/22</p>`
+    <p class="is-size-7 pb-5">Job posted: ${currentJob.post_date}</p>`
     ;
     applyButton = document.getElementById("apply-button");
     backButton = document.getElementById("back-button");
