f49f6335ee0cf2e0929d5b41db587238e2337ebf
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-05-12 11:06:16 -0400

    added timestamp to posts

--- public/js/viewmyjobs.js
+++ public/js/viewmyjobs.js
@@ -21,7 +21,7 @@ function getJobs() {
                     <div class="card">
 
                         <header class="card-header">
-                            <p class="card-header-title">${job.title}</p>
+                            <p class="card-header-title">${job.title} - posted: ${job.post_date}</p>
                         </header>
 
                         <div class="card-content">
